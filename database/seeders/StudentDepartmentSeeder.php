b48ca910c28527472c2db08ddbaff50dfc2c6655
RizkyGnw <rzkygnw@gmail.com>
2024-11-25 21:16:02 +0700

    Menambahkan seeder

---
+++ database/seeders/StudentDepartmentSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class StudentDepartmentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
