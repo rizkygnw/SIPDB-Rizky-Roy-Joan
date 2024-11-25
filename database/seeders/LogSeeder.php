2a67fbe0cb6575d6a86652124e778ffe49d78a4e
ROYJRDY <pontianakcity55555@gmail.com>
2024-11-25 21:28:27 +0700

    Menambahkan seeder PaymentSeeder dan LogSeeder

--- database/seeders/LogSeeder.php
+++ database/seeders/LogSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Log;
 
 class LogSeeder extends Seeder
 {
@@ -12,6 +13,30 @@ class LogSeeder extends Seeder
      */
     public function run(): void
     {
+        Log::create([
+            'user_id' => 1, // Assuming this user ID exists
+            'activity' => 'Login',
+        ]);
+
+        Log::create([
+            'user_id' => 2,
+            'activity' => 'Upload Document',
+        ]);
+
+        Log::create([
+            'user_id' => 3,
+            'activity' => 'Edit Profile',
+        ]);
+
+        Log::create([
+            'user_id' => 4,
+            'activity' => 'Login',
+        ]);
+
+        Log::create([
+            'user_id' => 5,
+            'activity' => 'Payment',
+        ]);
         //
     }
 }
--- database/seeders/PaymentSeeder.php
+++ database/seeders/PaymentSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Payment;
 
 class PaymentSeeder extends Seeder
 {
@@ -12,6 +13,40 @@ class PaymentSeeder extends Seeder
      */
     public function run(): void
     {
+        Payment::create([
+            'student_id' => 1,
+            'payment_date' => '2024-11-01',
+            'amount' => 100000,
+            'receipt_number' => 'INV001',
+        ]);
+
+        Payment::create([
+            'student_id' => 2,
+            'payment_date' => '2024-11-02',
+            'amount' => 120000,
+            'receipt_number' => 'INV002',
+        ]);
+
+        Payment::create([
+            'student_id' => 3,
+            'payment_date' => '2024-11-03',
+            'amount' => 110000,
+            'receipt_number' => 'INV003',
+        ]);
+
+        Payment::create([
+            'student_id' => 4,
+            'payment_date' => '2024-11-04',
+            'amount' => 130000,
+            'receipt_number' => 'INV004',
+        ]);
+
+        Payment::create([
+            'student_id' => 5,
+            'payment_date' => '2024-11-05',
+            'amount' => 125000,
+            'receipt_number' => 'INV005',
+        ]);
         //
     }
 }
